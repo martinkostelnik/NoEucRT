75b0e9616756df4384fb513f280359bc8287c0c6
Faz0lek <martinkostelnik0@gmail.com>
2020-12-09 17:07:44 +0100

    Implement basic functionality

---
+++ src/NoEucEngine.cpp
@@ -0,0 +1,49 @@
+#include "NoEucEngine.hpp"
+
+NoEucEngine::NoEucEngine() : window(sf::VideoMode(800, 600, 24), "Non-Euclidean Ray Tracer", sf::Style::None), renderer(), t(), s()
+{
+	t.create(800, 600);
+	s.setTexture(t);
+}
+
+int NoEucEngine::run()
+{
+	while (window.isOpen())
+	{
+		// Process events
+		handleEvents();
+
+		// Update stuff
+
+		// Render image
+		renderer.render(t);
+
+		// Display image
+		window.clear();
+		window.draw(s);
+		window.display();
+	}
+
+	return 0;
+}
+
+void NoEucEngine::handleEvents()
+{
+	sf::Event event;
+	while (window.pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::Closed:
+			window.close();
+			break;
+
+		case sf::Event::KeyPressed:
+			if (event.key.code == sf::Keyboard::Escape)
+			{
+				window.close();
+			}
+			break;
+		}
+	}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,8 @@
-#include <SFML/Graphics.hpp>
-#include <glm/glm.hpp>
+#include "NoEucEngine.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    NoEucEngine engine;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+    return engine.run();
 }
