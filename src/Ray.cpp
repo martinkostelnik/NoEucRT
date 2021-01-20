98e9464709718b66539e01d077b748f96bb3d328
Faz0lek <martinkostelnik0@gmail.com>
2021-01-20 21:30:26 +0100

    Switch from vec3 to vec4 model vertices

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -8,13 +8,13 @@ bool Ray::intersectsTriangle(const Triangle& triangle, float& out_distance)
 {
     const float EPSILON = 0.0000001;
 
-    glm::vec3 rayOrigin = { this->origin.x, this->origin.y, this->origin.z };
-    glm::vec3 rayDirection = { this->direction.x, this->direction.y, this->direction.z };
+    glm::vec3 origin3 = glm::vec3(origin.x, origin.y, origin.z);
+    glm::vec3 direction3 = glm::vec3(direction.x, direction.y, direction.z);
 
     glm::vec3 edge1 = triangle.v2 - triangle.v1;
     glm::vec3 edge2 = triangle.v3 - triangle.v1;
 
-    glm::vec3 h = glm::cross(rayDirection, edge2);
+    glm::vec3 h = glm::cross(direction3, edge2);
     float a = glm::dot(edge1, h);
 
     if (a > -EPSILON && a < EPSILON)
@@ -23,7 +23,7 @@ bool Ray::intersectsTriangle(const Triangle& triangle, float& out_distance)
     }
 
     float f = 1.0 / a;
-    glm::vec3 s = rayOrigin - triangle.v1;
+    glm::vec3 s = direction3 - triangle.v1;
     float u = f * glm::dot(s, h);
 
     if (u < 0.0 || u > 1.0)
@@ -32,7 +32,7 @@ bool Ray::intersectsTriangle(const Triangle& triangle, float& out_distance)
     }
 
     glm::vec3 q = glm::cross(s, edge1);
-    float v = f * glm::dot(rayDirection, q);
+    float v = f * glm::dot(direction3, q);
 
     if (v < 0.0 || u + v > 1.0)
     {
--- src/Scene.cpp
+++ src/Scene.cpp
@@ -3,18 +3,18 @@
 Scene::Scene() : objects(), mainCamera()
 {
 	/*Model testingTriangle;
-	testingTriangle.vertices = { {0, 0, -500}, {50, 0, -500}, {0, 50, -500} };
+	testingTriangle.vertices = { {0, 0, -500, 1}, {50, 0, -500, 1}, {0, 50, -500, 1} };
 	testingTriangle.indices = {0, 1, 2};
 	objects.push_back(testingTriangle);*/
 
 	/*Model testingSquare;
-	testingSquare.vertices = { {-100, -100, -100}, {0, -100, -100}, {0, 0, -100}, {-100, 0, -100}};
+	testingSquare.vertices = { {-100, -100, -100, 1}, {0, -100, -100, 1}, {0, 0, -100, 1}, {-100, 0, -100, 1}};
 	testingSquare.indices = { 0, 1, 2, 0, 2, 3 };
 	objects.push_back(testingSquare);*/
 
 	Model testingCube;
-	testingCube.vertices = { {-100, -100, -100}, {0, -100, -100}, {0, 0, -100}, {-100, 0, -100},
-							 {-100, -100, -200}, {0, -100, -200}, {0, 0, -200}, {-100, 0, -200} };
+	testingCube.vertices = { {-100, -100, -100, 1}, {0, -100, -100, 1}, {0, 0, -100, 1}, {-100, 0, -100, 1},
+							 {-100, -100, -200, 1}, {0, -100, -200, 1}, {0, 0, -200, 1}, {-100, 0, -200, 1} };
 	testingCube.indices = {0, 1, 2, 0, 2, 3, 0, 4, 3, 3, 4, 7, 4, 5, 6, 4, 6, 7, 1, 2, 5, 2, 5, 6, 0, 1, 4, 1, 4, 5, 2, 3, 7, 2, 6, 7};
 	objects.push_back(testingCube);
 }
