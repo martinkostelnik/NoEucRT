2f5a38b3e232c8216ee98d4dcb374e986e8b739f
Faz0lek <martinkostelnik0@gmail.com>
2021-01-22 15:03:27 +0100

    Add average fps counter

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -2,7 +2,7 @@
 
 Scene::Scene() : objects(), mainCamera()
 {
-	Model testingTriangle;
+	/*Model testingTriangle;
 	testingTriangle.vertices = { {0, 0, -500, 1}, {50, 0, -500, 1}, {0, 50, -500, 1} };
 	testingTriangle.indices = {0, 1, 2};
 	objects.push_back(testingTriangle);
@@ -10,7 +10,7 @@ Scene::Scene() : objects(), mainCamera()
 	Model testingSquare;
 	testingSquare.vertices = { {-100, -100, -100, 1}, {0, -100, -100, 1}, {0, 0, -100, 1}, {-100, 0, -100, 1}};
 	testingSquare.indices = { 0, 1, 2, 0, 2, 3 };
-	objects.push_back(testingSquare);
+	objects.push_back(testingSquare);*/
 
 	Model testingCube;
 	testingCube.vertices = { {-100, -100, -100, 1}, {0, -100, -100, 1}, {0, 0, -100, 1}, {-100, 0, -100, 1},
