d41ab3a206b1fecf849d05279313e6289f0f6340
Faz0lek <martinkostelnik0@gmail.com>
2021-01-31 20:33:30 +0100

    Make rendering parallel

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,6 +1,5 @@
 #include "Renderer.hpp"
 #include "Ray.hpp"
-#include <iostream>
 
 Renderer::Renderer()
 {
@@ -26,39 +25,26 @@ void Renderer::render(const Scene& scene, sf::Texture& texture)
 
 	// scale = tan(alpha/2 * pi/180)
 	float scale = tan(scene.mainCamera.fov * 0.00872665);
-
-	glm::vec4 d = { 0, 0, -1, 0 };
 	float distance = 0.0;
 	float minDistance = FLT_MAX;
 
-	for (size_t y = 0; y < size.y; y++)
+	#pragma omp parallel for private(distance, minDistance)
+	for (int y = 0; y < size.y; y++)
 	{
-		for (size_t x = 0; x < size.x; x++)
+		#pragma omp parallel for
+		for (int x = 0; x < size.x; x++)
 		{
 			Ray primaryRay({ (2 * (x + 0.5) / (float)size.x - 1) * scale * aspectRatio, (1 - 2 * (y + 0.5) / (float)size.y) * scale, -1, 0 });
 
 			// Transform ray origin using camera world transformation matrix
-			//ray.origin = scene.mainCamera.toWorld * ray.origin;
-			primaryRay.origin = {
-				primaryRay.origin.x * scene.mainCamera.toWorld[0].x + primaryRay.origin.y * scene.mainCamera.toWorld[1].x + primaryRay.origin.z * scene.mainCamera.toWorld[2].x + primaryRay.origin.w * scene.mainCamera.toWorld[3].x,
-				primaryRay.origin.x * scene.mainCamera.toWorld[0].y + primaryRay.origin.y * scene.mainCamera.toWorld[1].y + primaryRay.origin.z * scene.mainCamera.toWorld[2].y + primaryRay.origin.w * scene.mainCamera.toWorld[3].y,
-				primaryRay.origin.x * scene.mainCamera.toWorld[0].z + primaryRay.origin.y * scene.mainCamera.toWorld[1].z + primaryRay.origin.z * scene.mainCamera.toWorld[2].z + primaryRay.origin.w * scene.mainCamera.toWorld[3].z,
-				primaryRay.origin.x * scene.mainCamera.toWorld[0].w + primaryRay.origin.y * scene.mainCamera.toWorld[1].w + primaryRay.origin.z * scene.mainCamera.toWorld[2].w + primaryRay.origin.w * scene.mainCamera.toWorld[3].w
-			};
-
 			// Calculate ray direction and transform it using camera world transformation matrix
-			//ray.direction = scene.mainCamera.toWorld * d;
-			primaryRay.direction = {
-				primaryRay.direction.x * scene.mainCamera.toWorld[0].x + primaryRay.direction.y * scene.mainCamera.toWorld[1].x + primaryRay.direction.z * scene.mainCamera.toWorld[2].x + primaryRay.direction.w * scene.mainCamera.toWorld[3].x,
-				primaryRay.direction.x * scene.mainCamera.toWorld[0].y + primaryRay.direction.y * scene.mainCamera.toWorld[1].y + primaryRay.direction.z * scene.mainCamera.toWorld[2].y + primaryRay.direction.w * scene.mainCamera.toWorld[3].y,
-				primaryRay.direction.x * scene.mainCamera.toWorld[0].z + primaryRay.direction.y * scene.mainCamera.toWorld[1].z + primaryRay.direction.z * scene.mainCamera.toWorld[2].z + primaryRay.direction.w * scene.mainCamera.toWorld[3].z,
-				primaryRay.direction.x * scene.mainCamera.toWorld[0].w + primaryRay.direction.y * scene.mainCamera.toWorld[1].w + primaryRay.direction.z * scene.mainCamera.toWorld[2].w + primaryRay.direction.w * scene.mainCamera.toWorld[3].w
-			};
+			#pragma omp simd
+			for (int i = 0; i < 4; i++)
+			{
+				primaryRay.origin[i] = primaryRay.origin.x * scene.mainCamera.toWorld[0][i] + primaryRay.origin.y * scene.mainCamera.toWorld[1][i] + primaryRay.origin.z * scene.mainCamera.toWorld[2][i] + primaryRay.origin.w * scene.mainCamera.toWorld[3][i];
+				primaryRay.direction[i] = primaryRay.direction.x * scene.mainCamera.toWorld[0][i] + primaryRay.direction.y * scene.mainCamera.toWorld[1][i] + primaryRay.direction.z * scene.mainCamera.toWorld[2][i] + primaryRay.direction.w * scene.mainCamera.toWorld[3][i];
+			}
 
-			// Do we need to normalize?
-			//ray.direction = glm::normalize(ray.direction);
-
-			// Intersections
 			distance = 0.0;
 			minDistance = FLT_MAX;
 
@@ -73,15 +59,13 @@ void Renderer::render(const Scene& scene, sf::Texture& texture)
 							minDistance = distance;
 
 							// Shading
-							pixels[position] = 255;	// RED
-							pixels[position + 1] = 0;	// GREEN
-							pixels[position + 2] = 0;	// BLUE
+							pixels[((y * size.x) + x) * 4] = 255;	// RED
+							pixels[((y * size.x) + x + 1) * 4] = 0;	// GREEN
+							pixels[((y * size.x) + x + 2) * 4] = 0;	// BLUE
 						}
 					}
 				}
 			}
-
-			position += 4;
 		}
 	}
 	texture.update(pixels);
