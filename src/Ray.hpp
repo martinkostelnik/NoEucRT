1caf7e15a5cc4f7ffbfa11b8897eae95e2ec6233
Faz0lek <martinkostelnik0@gmail.com>
2021-02-08 16:24:39 +0100

    Implement ray-AABB intersection

--- src/Ray.hpp
+++ src/Ray.hpp
@@ -2,6 +2,7 @@
 
 #include <glm/glm.hpp>
 #include "Triangle.hpp"
+#include "AABB.hpp"
 
 class Ray
 {
@@ -13,4 +14,5 @@ public:
 	glm::vec4 direction;
 
 	bool intersectsTriangle(const Triangle& triangle, float& out_distance) const;
+	bool intersectsAABB(const AABB& aabb) const;
 };
