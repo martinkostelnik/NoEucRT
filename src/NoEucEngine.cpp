c0235f7d3f2005962241f2b15aa9deccbbae6135
Faz0lek <martinkostelnik0@gmail.com>
2021-04-25 00:19:27 +0200

    Change CMake to include font and skybox texture

--- src/NoEucEngine.cpp
+++ src/NoEucEngine.cpp
@@ -34,7 +34,7 @@ NoEucEngine::NoEucEngine() :
 	texture.create(width, height);
 	renderedImage.setTexture(texture);
 
-	font.loadFromFile("arial.ttf");
+	font.loadFromFile("resources/arial.ttf");
 	fpsText.setFont(font);
 	fpsText.setCharacterSize(18);
 	fpsText.setFillColor(sf::Color::Red);
