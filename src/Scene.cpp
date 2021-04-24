c903e3defad721a12d181496d7319054c9e5661c
Faz0lek <martinkostelnik0@gmail.com>
2021-04-24 18:56:22 +0200

    Create shrinking scene

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -384,3 +384,59 @@ Scene Scene::createLongTunnelScene()
 
 	return scene;
 }
+
+Scene Scene::createShrinkScene()
+{
+	Scene scene;
+	scene.name = "Shrinking tunnel";
+
+	/************************ Objects ************************/
+	std::unique_ptr<Model> floor{ new Model(Model::Type::Euclidean) };
+	floor->vertices = { {10000, -200, 10000, 1}, {10000, -200, -10000, 1}, {-10000, -200, -10000, 1}, {-10000, -200, 10000, 1} };
+	floor->indices = { 0, 1, 2, 0, 2, 3 };
+	floor->material.albedo = { 0.0f, 1.0f, 0.0f };
+	floor->material.kd = 1.0f;
+	floor->material.ks = 0.01f;
+	floor->material.shininess = 25.0f;
+	scene.objects.push_back(std::move(floor));
+	scene.floorLevel = -200;
+
+	std::unique_ptr<Model> TunnelOuter{ new Model(Model::Type::Euclidean) };
+	TunnelOuter->vertices = { { 500, 100, 0, 1 }, { 800, 100, 0, 1 }, { 500, 100, -600, 1 }, { 800, 100, -600, 1}, // Top Inner corners
+							 { 500, -200, 0, 1 }, { 800, -200, 0, 1 }, { 500, -200, -600, 1 }, { 800, -200, -600, 1}, // Bot Inner corners
+							 { 467, -200, 0, 1 }, { 833, -200, 0, 1 }, { 467, -200, -600, 1 }, { 833, -200, -600, 1 }, // Bot outer corners
+							 { 467, 133, 0, 1 }, { 833, 133, 0, 1 }, { 467, 133, -600, 1 }, { 833, 133, -600, 1 }, // Top outer corners
+							 { 500, 133, 0, 1 }, { 800, 133, 0, 1 }, { 500, 133, -600, 1 }, { 800, 133, -600, 1 } }; // Help vertices
+	TunnelOuter->indices = { 2, 0, 4, 2, 4, 6, 1, 3, 7, 1, 7, 5, 1, 0, 2, 1, 2, 3, 8, 12, 10, 12, 14, 10, 9, 15, 13, 9, 11, 15, 12, 13, 14, 13, 15, 14,
+							8, 4, 12, 4, 16, 12, 5, 9, 17, 9, 13, 17, 0, 1, 16, 1, 17, 16, 6, 10, 18, 10, 14, 18, 11, 7, 15, 7, 19, 15, 3, 2, 19, 2, 18, 19 };
+	TunnelOuter->material.albedo = { 0.0f, 0.0f, 1.0f };
+	TunnelOuter->material.kd = 1.0f;
+	TunnelOuter->material.ks = 0.0f;
+	TunnelOuter->material.shininess = 0.0f;
+	scene.objects.push_back(std::move(TunnelOuter));
+	/*********************************************************/
+
+	/************************* Tunnel ************************/
+	std::unique_ptr<ShrinkTunnel> tunnel{ new ShrinkTunnel() };
+	tunnel->vertices = { {495, -205, -5, 1 }, { 805, -205, -5, 1 }, { 495, 105, -5, 1 }, { 805, 105, -5, 1 },
+					  { 495, -205, -595, 1 }, { 805, -205, -595, 1 }, { 495, 105, -595, 1 }, { 805, 105, -595, 1 } };
+	tunnel->indices = { 0, 1, 2, 1, 3, 2, 4, 0, 2, 4, 2, 6, 5, 4, 6, 5, 6, 7, 5, 3, 1, 5, 7, 3, 6, 2, 7, 2, 3, 7, 5, 1, 0, 4, 5, 0 };
+	tunnel->direction = { 0.0f, 0.0f, -1.0f, 0.0f };
+	tunnel->finalSize = 0.5f;
+	tunnel->length = glm::distance(tunnel->vertices[0], tunnel->vertices[4]);
+	scene.objects.push_back(std::move(tunnel));
+	/*********************************************************/
+
+	/************************* Lights ************************/
+	Light l2({ 650, 70,-700, 1.0f }, 25000, { 1.0f, 1.0f, 1.0f }, Light::Type::Point);
+	scene.lights.push_back(l2);
+
+	Light l1({ 650, 70, 100, 1.0f }, 25000, { 1.0f, 1.0f, 1.0f }, Light::Type::Point);
+	scene.lights.push_back(l1);
+
+	Light sun({ 0, -1, 0, 0.0f }, 50, { 1.0f, 1.0f, 0.0f }, Light::Type::Distant);
+	scene.lights.push_back(sun);
+	/*********************************************************/
+
+	return scene;
+}
